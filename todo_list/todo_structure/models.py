fc89c1cf21ebd8bef26827167dd595c5b03bebdf
Владислав Кузьмин <sindbad1613@mail.ru>
2023-02-03 22:15:38 +0500

    Исправление ошибок

--- todo_list/todo_structure/models.py
+++ todo_list/todo_structure/models.py
@@ -2,6 +2,8 @@ import uuid
 
 from django.db import models
 
+from .constants import TEXT_LIMIT
+
 
 class Todo(models.Model):
     """Модель приложения Todo."""
@@ -9,10 +11,13 @@ class Todo(models.Model):
     uuid = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
-        max_length=8
+        editable=False
         )
     created = models.DateTimeField(
         'Дата создания', auto_now_add=True
     )
     body = models.CharField(max_length=2000)
     active = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.uuid[:TEXT_LIMIT] 
